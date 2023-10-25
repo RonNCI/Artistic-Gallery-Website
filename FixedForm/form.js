4e2e8df3a27d21fed671d092c4bf5c5616932c2c
RonNCI <130493006+RonNCI@users.noreply.github.com>
2023-10-25 12:54:47 +0100

    Add files via upload

---
+++ FixedForm/form.js
@@ -0,0 +1,45 @@
+const form = document.querySelector("form.application[name='contact-form']");
+const nameInput = document.querySelector("input.userDetails[name='name']");
+const emailInput = document.querySelector("input.userDetails[name='email']");
+const messageInput = document.querySelector("textarea.userQuestion[name='message']");
+
+nameInput.isValid = () => !!nameInput.value;
+emailInput.isValid = () => isValidEmail(emailInput.value);
+messageInput.isValid = () => !!messageInput.value;
+
+const inputFields = [nameInput, emailInput,  messageInput];
+
+const isValidEmail = (email) => {
+  const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+};
+
+let shouldValidate = false;
+let isFormValid = false;
+
+const validateInputs = () => {
+  console.log("we are here");
+  if (!shouldValidate) return;
+
+  isFormValid = true;
+  inputFields.forEach((input) => {
+    input.classList.remove("invalid");
+    input.nextElementSibling.classList.add("hide3");
+
+    if (!input.isValid()) {
+      input.classList.add("invalid");
+      isFormValid = false;
+      input.nextElementSibling.classList.remove("hide3");
+    }
+  });
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  shouldValidate = true;
+  validateInputs();
+  if (isFormValid) {
+  }
+});
+
+inputFields.forEach((input) => input.addEventListener("input", validateInputs));
---
+++ FullWebsite/external.js
@@ -0,0 +1,52 @@
+//creating a function to hide and show hideDiv2, hideGameContainer and hideGameResultContainer
+function hideDiv(){//name of function 
+  document.getElementById("hideDiv2");
+  //creating if to hide gameDiv1 and show gameDiv2
+  if(document.getElementById("hideDiv2").style.display == "none"){
+    document.getElementById("hideGameContainer").style.display = "none";
+    document.getElementById("hideDiv2").style.display ="block";
+    document.getElementById("hideGameResultContainer").style.display ="none";
+  }else{ //showing gameDiv1 and hiding gameDiv2 again
+    document.getElementById("hideGameContainer").style.display = "block";
+    document.getElementById("hideGameResultContainer").style.display ="block";
+    document.getElementById("hideDiv2").style.display = "none";
+  }
+}
+//creating random guessing game with Math.floor(Math.random())
+function playGuessingGame(){//name of function
+  //creating variables
+  var clickButton=document.getElementById('submitbttn');
+  var showOutPut=document.getElementById('showmsg');
+  var showOutPut2=document.getElementById('showmsg2');
+  var userGuesses = document.getElementById('userguess').value
+  var showCorrectGuess=document.getElementById('rightGuesses');
+  var showWrongGuess=document.getElementById('wrongGuesses');
+  var showRightNumber=document.getElementById('rightNumber');
+  var showRightPicture=document.getElementById('pictureNumber');
+  var pictureName = ["","Guardian Angel", "Naked Beauty", "Morder Art", "van Gogh", "Soul Eater", "Chaos"];
+  //creating variable for random number of 21
+  var randomNumGuess=[Math.floor(Math.random() * 6)];
+  //creating variable for results of guess
+  var resultsGuess=[0,0];
+
+  //creating output to show guess is right
+  if(userGuesses==randomNumGuess){
+    showOutPut.innerHTML='Great job!, you guessed the right number';
+    resultsGuess[0]++
+  //creating output if guess is too low
+  }else if(userGuesses<randomNumGuess){
+    showOutPut.innerHTML="Sorry, your guess was too low!";
+    showOutPut2.innerHTML="try again!";
+    resultsGuess[1]++;//if guess is wrong +1 wrongGuesses
+  //creating output if guess is too high
+  }else if(userGuesses>randomNumGuess){
+    showOutPut.innerHTML="Sorry, your guess was too low!";
+    showOutPut2.innerHTML="try again!";
+    resultsGuess[1]++;//if guess is wrong +1 wrongGuesses
+  }
+  //putting all results into the table
+  showCorrectGuess.innerHTML=resultsGuess[0];
+  showWrongGuess.innerHTML=resultsGuess[1];
+  showRightNumber.innerHTML=randomNumGuess;
+  showRightPicture.innerHTML=pictureName[userGuesses];
+ }
